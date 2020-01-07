d6f79f04b8bc9eb51cc33b4c6893620555addc4a
Michael Engleder <michael.engleder@o.roteskreuz.at>
2020-01-07 18:31:19 +0100

    First commit

---
+++ TravelCount.Contracts/IIdentifiyable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelCount.Contracts
+{
+    /// <summary>
+    /// Defines the basic properties of identifiyable components
+    /// </summary>
+    public partial interface IIdentifiyable
+    {
+        /// <summary>
+        /// Gets the identity of the component.
+        /// </summary>
+        public int Id { get; }
+    }
+}
